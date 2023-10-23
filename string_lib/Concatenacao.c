b174f6c086592a755caa9b654ca6a7d8b95c9d3b
Diablo <82063404+LucasDiabloChan@users.noreply.github.com>
2023-10-23 08:34:14 -0300

    Create Concatenacao.c

---
+++ string_lib/Concatenacao.c
@@ -0,0 +1,27 @@
+//@AsherLucRen
+// Concatenação de strings, com e sem a biblioteca >> string.h <<
+
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+
+// - - - - - - - - - - - - - - - - - - - - - 
+int main()
+{
+    setlocale(LC_ALL, "portuguese"); // formatação de caracteres em PT
+    
+    char palavra1_1[] = "Primeiro";
+    
+    char palavra1_2[10];
+    strcpy(palavra1_2, " Yameteee"); // espaço em branco proposital
+    
+    // Saída de dados
+    puts("___SAÍDA_DE_DADOS___");
+    
+    printf("Primeira string: %s\n", palavra1_1);
+    printf("Segunda string: %s\n", palavra1_2);
+    printf("Concatenação convencional: %s_%s\n", palavra1_1, palavra1_2);
+    printf("Concatenação convencional: %s\n", strcat(palavra1_1, palavra1_2));
+
+    return 0;
+}
