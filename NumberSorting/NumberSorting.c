f433a14443f0f16cf2890656e19e38e3ad8aa486
Diablo <82063404+LucasDiabloChan@users.noreply.github.com>
2023-11-11 16:58:10 -0300

    Update NumberSorting.c

--- NumberSorting/NumberSorting.c
+++ NumberSorting/NumberSorting.c
@@ -1,107 +1,83 @@
-// @AsherLucRen
-// Recebe 3 valores e retorna-os ordenados. Caso sejam iguais a função retorna 1, se distintos, retorna 0. 
+// @AsherLucRen 
+// Recebe 3 valores e retorna-os ordenados. Caso sejam iguais a função retorna 1 e retorna 0 se distintos.  
 
 #include <stdio.h>
 #include <locale.h>
+// = = = = = = = = = = = = = = 
 
-// = = = = = = = = = = = = = = = = = 
+int main() {
+    
+    float values[3], orderedValues[3];
+    
+    getValues(&values);
+    
+    puts("ANTES DE ORDENAR");
+    printf("1º pos: %f", values[0]);
+    printf("\n2º pos: %f", values[1]);
+    printf("\n3º pos: %f\n\n", values[2]);
+    
+    getOrderedValues(&values, &orderedValues);
+    
+    puts("DEPOIS DE ORDENAR");
+    printf("1º pos: %f", orderedValues[0]);
+    printf("\n2º pos: %f", orderedValues[1]);
+    printf("\n3º pos: %f", orderedValues[2]);
 
-void recebeValores();
-
-float valoresOrdenados[4];
-    // [0] - 0 ou 1, para distintos ou iguais
-    // [1] - menor valor
-    // [2] - valor intermediario
-    // [3] - maior valor
-
-int main()
-{
-    setlocale(LC_ALL, "portuguese");
-    
-    puts("_ _ _ _ORDENAÇÃO DE VALORES_ _ _ _");
-    
-    printf("\nDigite três (3) valores: \n");
-    recebeValores();
-    
-    if(valoresOrdenados[0] == 1)
-        printf("Valores iguais? Sério?");
-        
-    printf("Valores ordenados (decrescente): \n");
-    printf("1º valor: %.0f\n", valoresOrdenados[3]);
-    printf("2º valor: %.0f\n", valoresOrdenados[2]);
-    printf("3º valor: %.0f\n", valoresOrdenados[1]);
-    
-    
     return 0;
 }
 
-void recebeValores()
+void getValues(int *memToSave)
 {
-    float valor1, valor2, valor3;
-    
-    printf("\n1º valor: ");
-    scanf("%f", &valor1);
-    
-    printf("\n2º valor: ");
-    scanf("%f", &valor2);
-    
-    printf("\n3º valor: ");
-    scanf("%f", &valor3);
-    
-    if(valor1 != valor2 && valor1 != valor3)
-        valoresOrdenados[0] = 0;
-    else
-        valoresOrdenados[0] = 1;
-    
-    // menor valor:
-    if(valor1 < valor2)
-    {
-        if(valor1 < valor3)
-            valoresOrdenados[1] = valor1;
-        else
-            valoresOrdenados[1] = valor3;
-    }
-    else
-    {
-        if(valor2 < valor3)
-            valoresOrdenados[1] = valor2;
-        else
-            valoresOrdenados[1] = valor3;
-    }
-    
-    // maior valor:
-    if(valor3 > valor1)
-    {
-        if(valor3 > valor2)
-            valoresOrdenados[3] = valor3;
-        else
-            valoresOrdenados[3] = valor2;
-    }
-    else
-    {
-        if(valor1 > valor2)
-            valoresOrdenados[3] = valor1;
-        else
-            valoresOrdenados[3] = valor2;
-    }
-    
-    // valor intermediario:
-    if(valor1 > valoresOrdenados[1] && valor1 < valoresOrdenados[3])
-        valoresOrdenados[2] = valor1;
-    else if(valor2 > valoresOrdenados[1] && valor2 < valoresOrdenados[3])
-        valoresOrdenados[2] = valor2;
-    else
-        valoresOrdenados[2] = valor3;
+    puts("Insert three values above: ");
+    printf("1º - "); scanf("%f", &memToSave[0]);
+    printf("\n2º - "); scanf("%f", &memToSave[1]);
+    printf("\n3º - "); scanf("%f", &memToSave[2]);
 }
 
-
-
-
-
-
-
-
-
-
-
-
+void getOrderedValues(int *valuesToOrder, int *orderedValues)
+{
+    if(valuesToOrder[0] >= valuesToOrder[1]) // se 1º>2º
+    {
+        if(valuesToOrder[0] >= valuesToOrder[2]) // se 1º>3º
+            if(valuesToOrder[1] >= valuesToOrder[2]) // se 2º>3º
+            { // 1º >= 2º >= 3º
+                orderedValues[0] = valuesToOrder[0]; 
+                orderedValues[1] = valuesToOrder[1]; 
+                orderedValues[2] = valuesToOrder[2]; 
+            }
+            else
+            { // 1º >= 3º >= 2º
+                orderedValues[0] = valuesToOrder[0]; 
+                orderedValues[1] = valuesToOrder[2]; 
+                orderedValues[2] = valuesToOrder[1]; 
+            }
+        else 
+        { // 3º >= 1º >= 2º
+            orderedValues[0] = valuesToOrder[2];
+            orderedValues[1] = valuesToOrder[0];
+            orderedValues[2] = valuesToOrder[1];
+        }   
+    }
+    else // 2º > 1º
+    {
+        if(valuesToOrder[1] >= valuesToOrder[2]) // se 2º>3º
+            if(valuesToOrder[0] >= valuesToOrder[2]) // se 1º>3º
+            { // 2º >= 1º >= 3º
+                orderedValues[0] = valuesToOrder[1]; 
+                orderedValues[1] = valuesToOrder[0]; 
+                orderedValues[2] = valuesToOrder[2]; 
+            }
+            else // 2º >= 3º >= 1º
+            {
+                orderedValues[0] = valuesToOrder[1]; 
+                orderedValues[1] = valuesToOrder[2]; 
+                orderedValues[2] = valuesToOrder[0]; 
+            }
+        else // 3º >= 2º >= 1º
+        {
+            orderedValues[0] = valuesToOrder[2];
+            orderedValues[1] = valuesToOrder[1];
+            orderedValues[2] = valuesToOrder[0];
+        }
+    }
+}
